7d90f2f6b00df6dab29178c845e2339fb06371d2
Kamil Polowczyk <kamil.polowczyk.stud@pw.edu.pl>
2024-12-02 08:40:33 +0100

    lab6_v0

--- foo.hpp
+++ foo.hpp
@@ -10,7 +10,9 @@ std::vector< char > foo(std::list< Human >& people)
 {
     std::vector< char > retval(people.size());
 
-    // Twoja implementacja tutaj
+    std::for_each(people.begin(), people.end(), [](Human& human) { human.birthday(); });
+    std::transform(people.rbegin(), people.rend(), retval.begin(),
+                   [](const Human& human) { return human.isMonster() ? 'n' : 'y'; });
 
     return retval;
 }
